2d57bab44b727aa80ae600cfac4b986d53b41a71
Sergey Skumatov <sergeyskumatov@aol.com>
2020-04-01 08:48:05 -0600

    Count days - simple

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+// Chalange 1: Your Age in Days
+
+function ageInDays() {
+    var birthYear = prompt("What Year were you born... my friend?");
+    var ageInDayss = (2020-birthYear) * 365;
+    var h1 = document.createElement("h1");
+    var textAnswer = document.createTextNode("You are " + ageInDayss + " days old");
+    h1.setAttribute("id", "ageInDays");
+    h1.appendChild(textAnswer);
+    document.getElementById("flex-box-result").appendChild(h1);
+    // console.log(ageInDayss);
+}
+
+function reset() {
+    document.getElementById("ageInDays").remove();
+}
