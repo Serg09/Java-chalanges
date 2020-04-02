b4a1243d79bb70c2103f0bbb5fa8eb22f2662f6e
Sergey Skumatov <sergeyskumatov@aol.com>
2020-04-02 11:06:33 -0600

    Generate Cats )

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,15 @@ function ageInDays() {
 function reset() {
     document.getElementById("ageInDays").remove();
 }
+
+// Challenge 2: Generate Cats
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen'); // get access to "flex-box-container-2" in index.html
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
+
+function resetImage() {
+    document.getElementById("flex-cat-gen").remove();
+}
